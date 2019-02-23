11ac69320fa4f5006ce932810d1c67ebd6d1a1af
Stefan Ivic <nightwalkerkg@gmail.com>
2019-02-23 22:31:51 +0100

    refactor: implement editorconfig and organize code

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const discord = require("discord.js");
+const client = new discord.Client();
 const config = require("./config.json");
-// Messages
 const help = require("./messages/help.json");
 const welcome = require("./messages/welcome.json");
 
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
 });
 
 client.on("message", message => {
-  if (message.author.bot) return;
-  if (message.content.indexOf(config.prefix) !== 0) return;
-  if (!message.member.roles.some(r => ["Administrators"].includes(r.name)))
-    return message.reply("Sorry, you don't have permissions to use this!");
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+    if (!message.member.roles.some(r => ["Administrators"].includes(r.name)))
+        return message.reply("Sorry, you don't have permissions to use this!");
 
-  const args = message.content
-    .slice(config.prefix.length)
-    .trim()
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
+    const args = message.content
+        .slice(config.prefix.length)
+        .trim()
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-  if (command === "help") {
-    let [person, cmd] = args;
+    if (command === "help") {
+        let [person, msg] = args;
 
-    if (cmd === "channel") {
-      message.channel.send({
-        embed: {
-          color: 3447003,
-          description: `${person} ${help.channel}`
+        for (let cmd in help) {
+            if (help.hasOwnProperty(cmd)) {
+                if (msg === cmd) {
+                    message.channel.send({
+                        embed: help[cmd]
+                    });
+                }
+            }
         }
-      });
     }
-  }
 });
 
 client.on("guildMemberAdd", member => {
-  if (member.id)
-    client.users.get(member.id).send({
-      embed: {
-        color: 3447003,
-        description: welcome.message
-      }
-    });
+    if (member.id) {
+        client.users.get(member.id).send({
+            embed: {
+                color: 3447003,
+                description: welcome.message
+            }
+        });
+    }
 });
 
 client.login(config.token);
