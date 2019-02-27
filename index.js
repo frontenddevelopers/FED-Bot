0208300e675276d05657d040493fac6ea9128473
Stefan Ivic <nightwalkerkg@gmail.com>
2019-02-27 22:18:44 +0100

    Implements babel, prettier, eslint and adds mdn lookup

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const discord = require("discord.js");
+import discord from 'discord.js';
+import config from './config.json';
+import help from './messages/help.json';
+import welcome from './messages/welcome.json';
+import { searchMDN } from 'mdn-search-docs';
+
 const client = new discord.Client();
-const config = require("./config.json");
-const help = require("./messages/help.json");
-const welcome = require("./messages/welcome.json");
 
-client.on("ready", () => {
-    console.log("I am ready!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-
-client.on("message", message => {
+client.on('message', message => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
-    if (!message.member.roles.some(r => ["Administrators"].includes(r.name)))
+    if (!message.member.roles.some(r => ['Administrators'].includes(r.name)))
         return message.reply("Sorry, you don't have permissions to use this!");
 
     const args = message.content
@@ -20,8 +21,8 @@ client.on("message", message => {
         .split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if (command === "help") {
-        let [person, msg] = args;
+    if (command === 'help') {
+        let [...msg] = args;
 
         for (let cmd in help) {
             if (help.hasOwnProperty(cmd)) {
@@ -33,9 +34,52 @@ client.on("message", message => {
             }
         }
     }
+
+    if (command === 'mdn') {
+        let [...term] = args;
+        let result = '';
+        const collector = new discord.MessageCollector(
+            message.channel,
+            m => m.author.id === message.author.id,
+            { time: 10000 }
+        );
+        searchMDN({ term: term })
+            .then(res => {
+                res.documents.forEach((r, i) => {
+                    result += `${i + 1}) ${r.title}\n`;
+                });
+                message.channel.send('```css\n' + `${result}` + '```');
+                collector.on('collect', message => {
+                    if (
+                        !Number.isNaN(message.content) &&
+                        message.content <= 10
+                    ) {
+                        let test = res.documents[message.content - 1];
+
+                        message.channel.send({
+                            embed: {
+                                title: test.title,
+                                description: test.excerpt.replace(
+                                    /<\/?[^>]+>/gi,
+                                    ''
+                                ),
+                                url: test.url
+                            }
+                        });
+                    } else {
+                        message.channel.send(
+                            'Invalid number. Please select  a number from the list.'
+                        );
+                    }
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 });
 
-client.on("guildMemberAdd", member => {
+client.on('guildMemberAdd', member => {
     if (member.id) {
         client.users.get(member.id).send({
             embed: welcome.message
